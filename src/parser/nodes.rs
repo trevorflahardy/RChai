f56c0b6ba8b8a78eb1e28e5684c4efac752dd6e1
NextChai <futureengineerdlc@gmail.com>
2024-01-28 00:02:03 -0500

    Refactor node structs and traits

--- src/parser/nodes.rs
+++ src/parser/nodes.rs
@@ -2,32 +2,32 @@ use crate::lexer::Token;
 use std::boxed::Box;
 
 pub trait Node {}
-pub trait Expression: Node {}
-pub trait Statement: Node {}
+pub trait NodeExpression: Node {}
+pub trait NodeStatement: Node {}
 
-pub struct Module {
+pub struct NodeModule {
     pub body: Vec<Box<dyn Node>>,
 }
 
-pub struct Identifier<'a> {
+pub struct NodeIdentifier<'a> {
     pub name: &'a str,
     pub token: &'a Token<'a>,
 }
 
-impl Node for Identifier<'_> {}
+impl Node for NodeIdentifier<'_> {}
 
-pub struct Assignment<'a> {
-    pub targets: Vec<&'a Identifier<'a>>,
-    pub value: &'a dyn Expression,
+pub struct NodeAssignment<'a> {
+    pub identifier: &'a NodeIdentifier<'a>, // What is being assigned to.
+    pub expression: &'a dyn NodeExpression, // The expression being assigned.
 }
 
-impl Node for Assignment<'_> {}
+impl Node for NodeAssignment<'_> {}
 
 // Literals (int, string, and bool)
-pub struct Constant<'a> {
-    pub value: u32, // Will become dynamic later
+pub struct NodeConstant<'a> {
+    pub value: u32, // Will become dynamic later but only int supported
     pub token: &'a Token<'a>,
 }
 
-impl Node for Constant<'_> {}
-impl Expression for Constant<'_> {}
+impl Node for NodeConstant<'_> {}
+impl NodeExpression for NodeConstant<'_> {}
